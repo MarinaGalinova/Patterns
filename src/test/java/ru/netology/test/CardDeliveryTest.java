a2fc6d8ab764ceeb7c726180691d35b27a6993f8
MarinaGalinova <m.kriklivenko@gmail.com>
2023-07-16 17:34:28 +0300

    add test

---
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -0,0 +1,64 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+//import ru.netology.DataGenerator;
+import ru.netology.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardDeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan meeting")
+    void shouldSuccessfulPlanMeeting() {
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysToFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysToFirstMeeting);
+        int daysToSecondMeeting = 7;
+        String secondMeetingDate = DataGenerator.generateDate(daysToSecondMeeting);
+
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement] input").click();
+
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+
+        $("[data-test-id= 'success-notification'] notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(secondMeetingDate);
+        $(byText("Запланировать")).click();
+        $("[data-test-id= 'success-notification'] notification__content")
+                .shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id= 'success-notification'] notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(visible);
+    }
+}
+
+
+// Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
+// firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
+// generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
+// имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
